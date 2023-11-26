a900161c21aff360a44432503fbb175d74b057d6
JunQi123-del <tanjunqi47@gmail.com>
2023-11-26 16:23:29 +0800

    added resizing function

--- LoginPage/src/application/Main.java
+++ LoginPage/src/application/Main.java
@@ -1,11 +1,13 @@
 package application;
 	
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import listener.CloseWindowListener;
 
 public class Main extends Application {
 	
@@ -17,7 +19,11 @@ public class Main extends Application {
 			
 			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/LoginPageDialog.fxml"));
 			
-			Scene scene = new Scene(root);
+			GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+			int width = gd.getDisplayMode().getWidth();
+			int height = gd.getDisplayMode().getHeight();
+			
+			Scene scene = new Scene(root,width,height);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("LoginPage");
 			primaryStage.show();
